fbba0511830dad55f2adbe0ddcdf56d841b73655
YugyeongJung <63027011+YugyeongJung@users.noreply.github.com>
2023-11-25 18:33:37 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import { Route, Navigate, Routes, BrowserRouter } from 'react-router-dom';
+
 import './App.css';
+import Home from './component/Home';
+import ModelCheck from './component/ModelCheck'
+import TrainingResult from './component/TrainingResult'
+import UnlearningProgress from './component/UnlearningProgress'
+import UnlearningResult from './component/UnlearningResult'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Routes>
+					<Route exact path="/" element={<Navigate to="/Home" />} />
+					<Route path="/Home" element={<Home />} />
+          <Route path="/ModelCheck" element={<ModelCheck />} />
+          <Route path="/TrainingResult" element={<TrainingResult/>} />
+          <Route path="/UnlearningProgress" element={<UnlearningProgress/>}/>
+          <Route path="/UnlearningResult" element={<UnlearningResult/>}/>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
